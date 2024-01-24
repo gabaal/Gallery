3be74cfffe7cb36bef5a5ac069d2780a3fe6e392
gabaal <geraldbaalham@live.co.uk>
2024-01-24 08:14:52 +0000

    Back to the basic search and one image only

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Gallery
+
+const apiKey = "yatmYfJkdbM9yR9r8Icqdx7Gp_Xn3jlv1uzU5tMH8Ig"
+&orientation=landscape
--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Working");
+const accessKey = "yatmYfJkdbM9yR9r8Icqdx7Gp_Xn3jlv1uzU5tMH8Ig";
+
+const form = document.getElementById("form");
+const imagecontainer = document.getElementById("img-container");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event);
+  let query = event.target.input.value;
+  search(query);
+});
+
+async function search(queryParam) {
+  let response = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=yatmYfJkdbM9yR9r8Icqdx7Gp_Xn3jlv1uzU5tMH8Ig`
+  );
+  console.log(response);
+  let data = await response.json();
+  console.log(data.results[0].urls.raw);
+
+  let img = document.createElement("img");
+  img.src = data.results[0].urls.raw;
+
+  imagecontainer.appendChild(img);
+}
